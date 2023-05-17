d2ba59bc080341752c65269efcdb1016c1b43310
Crowdbotics <team@crowdbotics.com>
2023-05-17 18:05:50 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "masters-of-taste-40781",
+  url: "https://masters-of-taste-40781.botics.co",
+  api: "https://masters-of-taste-40781.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
